db0574d699ad66e6ac28e4703673aa1637e6f2b7
Quasimodo <47749932+QuasimodoPrograms@users.noreply.github.com>
2021-07-03 15:45:14 +0300

    1. Splash Screen

---
+++ DBelign/DBelign/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace DBelign
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create a new splash screen from a specified image
+            SplashScreen splash = new SplashScreen("/Images/splash.png");
+            splash.Show(false);
+            Thread.Sleep(500);
+
+            splash.Close(new TimeSpan(0, 0, 1));
+        }
+    }
+}
